68ba61a5bbda7e6293f142041373b866a7f3c4ff
lumian <821253522@qq.com>
2019-06-09 03:58:03 +0800

    更改表结构

--- src/main/resources/sql/jyxw.sql
+++ src/main/resources/sql/jyxw.sql
@@ -1,50 +1,46 @@
 /*
- Navicat Premium Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : 111
- Source Server Type    : MySQL
- Source Server Version : 80013
- Source Host           : localhost:3306
- Source Schema         : test
+Source Server         : lum
+Source Server Version : 50527
+Source Host           : localhost:3306
+Source Database       : test
 
- Target Server Type    : MySQL
- Target Server Version : 80013
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 50527
+File Encoding         : 65001
 
- Date: 06/06/2019 21:16:27
+Date: 2019-06-09 03:57:16
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
 -- Table structure for jyxw
 -- ----------------------------
 DROP TABLE IF EXISTS `jyxw`;
-CREATE TABLE `jyxw`  (
+CREATE TABLE `jyxw` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `JJMC` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `JJJC` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `JJZDM` int(8) NOT NULL,
-  `QSname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `QSnum` int(3) NULL DEFAULT NULL,
-  `GPJE` double(20, 0) NULL DEFAULT NULL,
-  `GPJEZB` double(5, 0) NULL DEFAULT NULL,
-  `QSJE` double(20, 0) NULL DEFAULT NULL,
-  `QSJEZB` double(5, 0) NULL DEFAULT NULL,
-  `ZQJE` double(20, 0) NULL DEFAULT NULL,
-  `ZQJEZB` double(5, 0) NULL DEFAULT NULL,
-  `HGJE` double(20, 0) NULL DEFAULT NULL,
-  `HGJEZB` double(5, 0) NULL DEFAULT NULL,
-  `QZJE` double(20, 0) NULL DEFAULT NULL,
-  `QZJEZB` double(5, 0) NULL DEFAULT NULL,
-  `JJJE` double(20, 0) NULL DEFAULT NULL,
-  `JJJEZB` double(5, 0) NULL DEFAULT NULL,
-  `QHJE` double(20, 0) NULL DEFAULT NULL,
-  `QHJEZB` double(5, 0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-
+  `JJMC` varchar(255) DEFAULT NULL,
+  `JJJC` varchar(255) DEFAULT NULL,
+  `JJZDM` varchar(8) NOT NULL,
+  `NUM` int(3) NOT NULL,
+  `QSname` varchar(255) NOT NULL,
+  `QSnum` int(3) DEFAULT NULL,
+  `GPJE` double(20,2) DEFAULT NULL,
+  `GPJEZB` double(5,2) DEFAULT NULL,
+  `QSJE` double(20,2) DEFAULT NULL,
+  `QSJEZB` double(5,2) DEFAULT NULL,
+  `ZQJE` double(20,2) DEFAULT NULL,
+  `ZQJEZB` double(5,2) DEFAULT NULL,
+  `HGJE` double(20,2) DEFAULT NULL,
+  `HGJEZB` double(5,2) DEFAULT NULL,
+  `QZJE` double(20,2) DEFAULT NULL,
+  `QZJEZB` double(5,2) DEFAULT NULL,
+  `JJJE` double(20,2) DEFAULT NULL,
+  `JJJEZB` double(5,2) DEFAULT NULL,
+  `QHJE` double(20,2) DEFAULT NULL,
+  `QHJEZB` double(5,2) DEFAULT NULL,
+  `XGSJ` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`,`JJZDM`,`QSname`)
+) ENGINE=InnoDB AUTO_INCREMENT=1040 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
